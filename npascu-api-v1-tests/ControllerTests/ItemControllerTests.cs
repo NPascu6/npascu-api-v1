dd274974f60a450ad3f0bc79b4ef37deb64ea9df
Norbert Pascu <58431719+NPascu6@users.noreply.github.com>
2023-11-04 23:43:43 +0100

    Added tests.

--- npascu-api-v1-tests/ControllerTests/ItemControllerTests.cs
+++ npascu-api-v1-tests/ControllerTests/ItemControllerTests.cs
@@ -1,12 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using npascu_api_v1.Controllers;
+using npascu_api_v1.Models.DTOs;
+using npascu_api_v1.Services.Interface;
+using NUnit.Framework;
 
 namespace npascu_api_v1_tests.ControllerTests
 {
-    internal class ItemControllerTests
+    [TestFixture]
+    public class ItemControllerTests
     {
+        private ItemController _controller;
+        private Mock<ILogger<ItemController>> _loggerMock;
+        private Mock<IItemService> _itemServiceMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<ItemController>>();
+            _itemServiceMock = new Mock<IItemService>();
+            _controller = new ItemController(_loggerMock.Object, _itemServiceMock.Object);
+        }
+
+        [Test]
+        public void GetItems_ReturnsEmptyList()
+        {
+            _itemServiceMock.Setup(service => service.GetItems()).Returns(new List<ItemDto>());
+
+            var result = _controller.GetItems();
+
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.IsEmpty((IEnumerable<ItemDto>)okResult.Value);
+        }
+
+        [Test]
+        public void GetItems_ReturnsItems()
+        {
+            var expectedItems = new List<ItemDto> { new ItemDto { Id = 1, Name = "Item1" } };
+            _itemServiceMock.Setup(service => service.GetItems()).Returns(expectedItems);
+
+            var result = _controller.GetItems();
+
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.AreEqual(expectedItems, (IEnumerable<ItemDto>)okResult.Value);
+        }
+
+        [Test]
+        public void CreateItem_ReturnsCreated()
+        {
+            var newItem = new ItemDto { Name = "NewItem" };
+            var createdItem = new ItemDto { Id = 1, Name = "NewItem" };
+            _itemServiceMock.Setup(service => service.CreateItem(newItem)).Returns(createdItem);
+
+            var result = _controller.CreateItem(newItem);
+
+            var okResult = (OkObjectResult)result.Result;
+            Assert.IsAssignableFrom<ItemDto>(okResult.Value);
+            Assert.AreEqual(createdItem, (ItemDto)okResult.Value);
+        }
+
+        [Test]
+        public void UpdateItem_ReturnsUpdatedItem()
+        {
+            int itemId = 1;
+            var itemDto = new ItemDto { Id = itemId, Name = "UpdatedItem" };
+            _itemServiceMock.Setup(service => service.UpdateItem(itemId, itemDto)).Returns(itemDto);
+
+            var result = _controller.UpdateItem(itemId, itemDto);
+
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.IsAssignableFrom<ItemDto>(okResult.Value);
+            Assert.AreEqual(itemDto, (ItemDto)okResult.Value);
+        }
+
+        [Test]
+        public void UpdateItem_ReturnsNotFound()
+        {
+            int itemId = 1;
+            var itemDto = new ItemDto { Id = itemId, Name = "UpdatedItem" };
+            _itemServiceMock.Setup(service => service.UpdateItem(itemId, itemDto)).Returns((ItemDto)null);
+
+            var result = _controller.UpdateItem(itemId, itemDto);
+
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
+        }
+
+        [Test]
+        public void DeleteItem_ReturnsNoContent()
+        {
+            int itemId = 1;
+            _itemServiceMock.Setup(service => service.DeleteItem(itemId)).Returns(true);
+
+            var result = _controller.DeleteItem(itemId);
+
+            Assert.IsInstanceOf<NoContentResult>(result);
+        }
+
+        [Test]
+        public void DeleteItem_ReturnsNotFound()
+        {
+            int itemId = 1;
+            _itemServiceMock.Setup(service => service.DeleteItem(itemId)).Returns(false);
+
+            var result = _controller.DeleteItem(itemId);
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        // Add more test methods for other ItemController actions, including error cases
     }
 }
--- npascu-api-v1-tests/ControllerTests/UserControllerTests.cs
+++ npascu-api-v1-tests/ControllerTests/UserControllerTests.cs
@@ -1,12 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using npascu_api_v1.Controllers;
+using npascu_api_v1.Models.DTOs;
+using npascu_api_v1.Services.Interface;
+using NUnit.Framework;
 
 namespace npascu_api_v1_tests.ControllerTests
 {
-    internal class UserControllerTests
+    [TestFixture]
+    public class UserControllerTests
     {
+        private UserController _controller;
+        private Mock<ILogger<UserController>> _loggerMock;
+        private Mock<IUserService> _userServiceMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<UserController>>();
+            _userServiceMock = new Mock<IUserService>();
+            _controller = new UserController(_loggerMock.Object, _userServiceMock.Object);
+        }
+
+        [Test]
+        public void GetUsers_ReturnsEmptyList()
+        {
+            // Arrange
+            _userServiceMock.Setup(service => service.GetUsers()).Returns(new List<UserDto>());
+
+            // Act
+            var result = _controller.GetUsers();
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.That(okResult, Is.Not.Null);
+        }
+
+        [Test]
+        public void GetUsers_ReturnsUsers()
+        {
+            // Arrange
+            var expectedUsers = new List<UserDto> { new UserDto { Id = 1, FirstName = "User1" } };
+            _userServiceMock.Setup(service => service.GetUsers()).Returns(expectedUsers);
+
+            // Act
+            var result = _controller.GetUsers();
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.AreEqual(expectedUsers, (IEnumerable<UserDto>)okResult.Value);
+        }
+
+        [Test]
+        public void CreateUser_ReturnsCreated()
+        {
+            var newUser = new UserDto { FirstName = "NewUser" };
+            var createdUser = new UserDto { Id = 1, FirstName = "NewUser" };
+            _userServiceMock.Setup(service => service.CreateUser(newUser)).Returns(createdUser);
+
+            var result = _controller.CreateUser(newUser);
+
+            Assert.IsNotNull(result.Result);
+            var createdAtActionResult = (CreatedAtActionResult)result.Result;
+            UserDto createUser = (UserDto)createdAtActionResult.Value;
+            Assert.AreEqual(createdUser.Id, createUser.Id);
+        }
+
+        [Test]
+        public void GetUser_ReturnsUser()
+        {
+            int userId = 1;
+            var user = new UserDto { Id = userId, FirstName = "User1" };
+            _userServiceMock.Setup(service => service.GetUserById(userId)).Returns(user);
+
+            var result = _controller.GetUser(userId);
+
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.IsAssignableFrom<UserDto>(okResult.Value);
+            Assert.AreEqual(user, (UserDto)okResult.Value);
+        }
+
+        [Test]
+        public void GetUser_ReturnsNotFound()
+        {
+            int userId = 1;
+            _userServiceMock.Setup(service => service.GetUserById(userId)).Returns((UserDto)null);
+
+            var result = _controller.GetUser(userId);
+
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
+        }
+
+        [Test]
+        public void UpdateUser_ReturnsUpdatedUser()
+        {
+            int userId = 1;
+            var userDto = new UserDto { Id = userId, FirstName = "UpdatedUser" };
+            _userServiceMock.Setup(service => service.UpdateUser(userId, userDto)).Returns(userDto);
+
+            var result = _controller.UpdateUser(userId, userDto);
+
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.IsAssignableFrom<UserDto>(okResult.Value);
+            Assert.AreEqual(userDto, (UserDto)okResult.Value);
+        }
+
+        [Test]
+        public void UpdateUser_ReturnsNotFound()
+        {
+            int userId = 1;
+            var userDto = new UserDto { Id = userId, FirstName = "UpdatedUser" };
+            _userServiceMock.Setup(service => service.UpdateUser(userId, userDto)).Returns((UserDto)null);
+
+            var result = _controller.UpdateUser(userId, userDto);
+
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
+        }
+
+        [Test]
+        public void DeleteUser_ReturnsNoContent()
+        {
+            int userId = 1;
+            _userServiceMock.Setup(service => service.DeleteUser(userId)).Returns(true);
+
+            var result = _controller.DeleteUser(userId);
+
+            Assert.IsInstanceOf<NoContentResult>(result);
+        }
+
+        [Test]
+        public void DeleteUser_ReturnsNotFound()
+        {
+            int userId = 1;
+            _userServiceMock.Setup(service => service.DeleteUser(userId)).Returns(false);
+
+            var result = _controller.DeleteUser(userId);
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        // Add more test methods for other UserController actions, including error cases
     }
 }
