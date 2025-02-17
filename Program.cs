d212a51e2c047061566af2ab191bc9e477e90ea3
Norbert Pascu <norbert.pascu@mavx.com>
2025-02-17 15:53:51 +0100

    Prgroam

--- Program.cs
+++ Program.cs
@@ -69,12 +69,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseRouting();
 app.UseAuthentication();
