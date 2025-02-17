30af3183f6010bfe1edcddb49dc7a56e7fce3a03
Norbert Pascu <norbert.pascu@mavx.com>
2025-02-17 14:23:11 +0100

    dockerfile

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
