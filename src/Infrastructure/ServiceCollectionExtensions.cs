92654b80cd6e3c29f4a2a4debc84b81378f9f1ac
Norbert Pascu <58431719+NPascu6@users.noreply.github.com>
2025-08-10 16:59:51 +0200

    feat: configure connection string from environment

--- src/Infrastructure/ServiceCollectionExtensions.cs
+++ src/Infrastructure/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,8 +9,11 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
     {
+        var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+                               ?? config.GetConnectionString("Postgres");
+
         services.AddDbContext<AppDbContext>(options =>
-            options.UseNpgsql(config.GetConnectionString("Postgres")));
+            options.UseNpgsql(connectionString));
         return services;
     }
 }
